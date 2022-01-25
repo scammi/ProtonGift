28df4371966a1f91d4a1a19f9c86de44b82c4855
scammi <scammi@gmail.com>
2022-01-25 11:06:42 -0300

    Create proton and retreave token uri

--- present.js
+++ present.js
@@ -7,13 +7,31 @@ const prontonAbi =  require('@charged-particles/protocol-subgraph/abis/Proton');
 (async ()=>{
   const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL);
   const signer = provider.getSigner()
+
+  const walletMnemonic = new ethers.Wallet.fromMnemonic(process.env.MEMONIC)
+  const wallet = walletMnemonic.connect(provider)
+
   // console.log(kovanAddresses.proton.address);
   // console.log(await provider.getBlockNumber())
 
   const protonContract = new ethers.Contract(kovanAddresses.proton.address, prontonAbi, provider);
-
   const supply = await protonContract.totalSupply();
 
+  // const newProtonTrx = await protonContract.connect(wallet).createBasicProton(
+  //   '0x7856C7F4A4Fa0a65Ed2950FbAcFEf7ACB17555E6',
+  //   '0x7856C7F4A4Fa0a65Ed2950FbAcFEf7ACB17555E6',
+  //   "https://gateway.pinata.cloud/ipfs/QmQxDjEhnYP6QAtLRyLV9N7dn1kDigz7iWnx5psmyXqy35/1",
+  // );
 
-  console.log(supply.toNumber())
+  // const newProton = await newProtonTrx.wait() ;
+
+  // console.log(newProtonTrx, newProton);
+  // console.log(supply.toNumber()) 
+
+  const ownerOf = await protonContract.creatorOf(10);
+  console.log(ownerOf);
+
+  const tokenUri = await protonContract.tokenURI(10);
+
+  console.log(tokenUri);
 })();
